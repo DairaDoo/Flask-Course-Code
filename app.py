a8a2e32be4801e2a391f13f96e74abe3adab9c95
DairanDoo <your_email@example.com>
2024-06-11 22:02:36 -0400

    Connection a base de datos store-db (recording) realizada con éxito y migraciones también utilizando render para alojar la base de datos local en postgreSQL.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
+from dotenv import load_dotenv
 
 # importamos la base de datos db ( que es SQLAlchemy )
 from db import db
@@ -18,6 +19,7 @@ from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
@@ -26,7 +28,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DABASE_URL", "sqlite:///data.db") # crea un archivo data.db
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db") # crea un archivo data.db
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     
